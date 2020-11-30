b2626979de6cb2d75c0a15a9cb1933f29428000d
Eleni Mandilara <madelinegr@gmail.com>
2020-11-30 15:08:27 +0100

    Initial commit

---
+++ Assets/Navigation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class Navigation : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    // Start is called before the first frame update
+    void Start() {
+        actions.Add("go ahead", Forward);
+        actions.Add("go back", Back);
+        actions.Add("turn right", TurnRight);
+        actions.Add("turn left", TurnLeft);
+
+        string[] keywords = {"go ahead", "go back", "turn right", "turn left"};
+
+        keywordRecognizer = new KeywordRecognizer(keywords);
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech; 
+        keywordRecognizer.Start();
+    }
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech) {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Forward() {
+        transform.Translate(0,0,-1);
+    }
+
+    private void Back() {
+        transform.Translate(0,0,1);
+    }
+
+    private void TurnRight() {
+        transform.Rotate(0,90,0);
+    }
+
+    private void TurnLeft() {
+        transform.Rotate(0,-90,0);
+    }
+
+    // Update is called once per frame
+    void Update() {}
+}
---
+++ Assets/VoiceMovement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceMovement : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    // Start is called before the first frame update
+    void Start() {
+        actions.Add("go forward", Forward);
+        actions.Add("go up", Up);
+        actions.Add("go down", Down);
+        actions.Add("go back", Back);
+
+        string[] keywords = {"go forward", "go up", "go down", "go back"};
+
+        keywordRecognizer = new KeywordRecognizer(keywords);
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech; 
+        keywordRecognizer.Start();
+    }
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech) {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Forward() {
+        transform.Translate(1,0,0);
+    }
+
+    private void Back() {
+        transform.Translate(-1,0,0);
+    }
+    
+    private void Up() {
+        transform.Translate(0,1,0);
+    }
+      
+    private void Down() {
+        transform.Translate(0,-1,0);
+    }
+
+    // Update is called once per frame
+    void Update() {}
+}
